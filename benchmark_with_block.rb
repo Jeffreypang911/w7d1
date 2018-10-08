de9d1aad4a573752a717e2c35f34a59c51fe236f
jeffreypang911 <jeffreypang911@gmail.com>
2018-10-08 19:02:30 +0000

    Benchmark Assignment

---
+++ benchmark_with_block.rb
@@ -0,0 +1,18 @@
+
+
+def benchmark
+  start_time = Time.now
+  yield
+  
+  end_time = Time.now
+  running_time = end_time - start_time
+  # This will return the difference in the timestamps in seconds
+end
+ 
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple"*100000000
+ 
+running_time = benchmark { long_string.reverse }
+ 
+puts "string.reverse took #{running_time} seconds to run"
